f04cf8271a172847af46f111d4beca3e3c849065
priyangshu24 <138028617+priyangshu24@users.noreply.github.com>
2024-12-29 18:29:30 +0530

    Add menu in responsive and complete the footer

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,32 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './Header.css'
+import React, { useState } from 'react';
+import './Header.css';
 import { TiThMenu } from "react-icons/ti";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <section className='h-wrapper'>
       <div className="flexCenter padding innerWidth h-container">
-
         <img src="./img/logo.png" alt="logo" width={100} />
-
-        <div className="h-menu" >
-          <a 
-          href="">
-            Residencies
-          </a>
+        
+        <div className={`h-menu ${menuOpen ? 'active' : ''}`}>
+          <a href="">Residencies</a>
           <a href="">Our Value</a>
           <a href="">Contact Us</a>
           <a href="">Get Started</a>
           <button className="button">
-            <a href="">Conactact</a>
+            <a href="">Contact</a>
           </button>
         </div>
       </div>
-      <div className='menu-icon'>
+      
+      <div className='menu-icon' onClick={toggleMenu}>
+        <TiThMenu size={30} color='white' />
       </div>
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
